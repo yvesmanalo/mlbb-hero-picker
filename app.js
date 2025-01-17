e5dbae9ba7627c4603f911e07a8d8f3eedc424a8
yvesmanalo <23yvesmanalo@gmail.com>
2025-01-17 19:47:53 +0800

    initial codes for the app

---
+++ app.js
@@ -0,0 +1,40 @@
+const heroName = document.querySelector('.hero');
+const heroRole = document.querySelector('.hero-role');
+const btn = document.querySelector('.btn');
+
+heroList = [{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+  hero: "Miya",
+  role: "Marksman"
+},{
+}]
+
+const pick = btn.addEventListener("click",function(){
+  
+})
